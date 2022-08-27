8b5bc0a39ef9e8ea3fdbcd8ade4c9e8fb0e6efec
PS PC <dynamicprogrammer307@gmail.com>
2022-08-27 16:21:28 +0530

    This is first commit

---
+++ src/main/java/com/springboot/jenkins/demo/SpringBootWithJenkinsApplication.java
@@ -0,0 +1,35 @@
+package com.springboot.jenkins.demo;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithJenkinsApplication {
+
+	public static Logger logger = LoggerFactory.getLogger(SpringBootWithJenkinsApplication.class);	
+	
+	@PostConstruct // Execute only once after the bean initialized
+	public void initMethod()
+	{
+		logger.info("Application Started...");
+	}
+	
+	
+	@PreDestroy // Execute only once after the bean destroyed from Application context
+	public void destrolMethod()
+	{
+		logger.info("Application Stoped...");
+	}
+	
+	
+	public static void main(String[] args) {
+		logger.info("Application executed...");
+		SpringApplication.run(SpringBootWithJenkinsApplication.class, args);
+	}
+
+}
